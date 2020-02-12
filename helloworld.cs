986498cb0008afffcde89d7ac1a803f6bdc0fd63
Kamogelo <kamogeloCharmaine142@gmail.com>
2020-02-12 05:02:48 +0200

    Create helloworld.cs

---
+++ helloworld.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+
+
+            Console.ReadKey();
+
+        }
+
+     
+
+    }
+}
